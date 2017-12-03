c3b7ed188809d35455eb9a14f84301f82b03071c
Christian Berendt <berendt@betacloud-solutions.de>
2017-12-03 18:11:29 +0100

    Prepare/use virtual environment in scripts

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+command -v pip >/dev/null 2>&1 || { echo >&2 "pip not installed"; exit 1; }
+command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv not installed"; exit 1; }
+
+if [[ ! -e .venv ]]; then
+    virtualenv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+
 python src/images.py
--- scripts/update-roles.sh
+++ scripts/update-roles.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+command -v pip >/dev/null 2>&1 || { echo >&2 "pip not installed"; exit 1; }
+command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv not installed"; exit 1; }
+
+if [[ ! -e .venv ]]; then
+    virtualenv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+
 python src/roles.py > roles.lst
 
 if [[ -s roles.lst ]]; then
@@ -12,4 +23,5 @@ if [[ -s roles.lst ]]; then
   git commit -F commit.msg
   git push
 fi
+
 rm -f roles.lst commit.msg
