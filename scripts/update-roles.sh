bd2e9da04a4d252b86922f2e7fc54d4b95e6ff0c
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-07 01:57:37 +0100

    Do not create venvs on travis

--- scripts/update-roles.sh
+++ scripts/update-roles.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-command -v pip >/dev/null 2>&1 || { echo >&2 "pip not installed"; exit 1; }
-command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv not installed"; exit 1; }
+if [[ $TRAVIS != "true" ]]; then
 
-if [[ ! -e .venv ]]; then
-    virtualenv .venv
-    source .venv/bin/activate
-    pip install -r requirements.txt
-else
+    command -v pip >/dev/null 2>&1 || { echo >&2 "pip not installed"; exit 1; }
+    command -v virtualenv >/dev/null 2>&1 || { echo >&2 "virtualenv not installed"; exit 1; }
+
+    if [[ ! -e .venv ]]; then
+        virtualenv .venv
+        pip install -r requirements.txt
+    fi
+
     source .venv/bin/activate
 fi
 
