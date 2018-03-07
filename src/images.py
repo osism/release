4fed5a422d148326421a13456958ced85be5c714
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-07 01:33:42 +0100

    Add a latest images for image clones

--- src/images.py
+++ src/images.py
@@ -20,63 +20,70 @@ import docker
 import json
 import yaml
 
+DOCKER_CLIENT = docker.APIClient(base_url='unix:///var/run/docker.sock')
+
 IMAGES = os.environ.get("IMAGES", None)
 if IMAGES:
     IMAGES = IMAGES.split(",")
 
+def process(version):
+
+    with open("etc/images.yml", "rb") as fp:
+        images = yaml.load(fp)
+
+    all_docker_images = []
+    repository_version = 'latest'
+    for filename in glob.glob("%s/*.yml" % version):
+        with open(filename, "rb") as fp:
+            versions = yaml.load(fp)
+            all_docker_images.append(versions.get('docker_images', {}))
+            if os.path.basename(filename) == 'base.yml' and version != 'latest':
+                repository_version = versions['repository_version']
+
+    for docker_images in all_docker_images:
+        for image in docker_images:
+            if IMAGES and image not in IMAGES:
+                print("skipping %s" % image)
+                continue
+
+            if image in ['rally', 'kolla-ansible', 'ceph-ansible', 'osism-ansible']:
+                continue
+
+            if not images[image][:5] == 'osism':
+                if image == 'ceph':
+                    target = 'osism/ceph-daemon'
+                else:
+                    target = "osism/" + images[image][images[image].find('/') + 1:]
+            else:
+                target = images[image]
+
+            target_tag = repository_version
+            if image in ['cephclient', 'openstackclient', 'ceph']:
+                target_tag = docker_images[image] + '-' + target_tag
+
+            source_tag = docker_images[image]
+            if image == 'ceph':
+                source_tag = "tag-build-master-%s-ubuntu-16.04" % source_tag
+
+            print("pulling - %s:%s" % (images[image], source_tag))
+            DOCKER_CLIENT.pull(images[image], source_tag)
+
+            print("tagging - %s:%s" % (target, target_tag))
+            DOCKER_CLIENT.tag("%s:%s" % (images[image], source_tag), target, target_tag)
+
+            print("pushing - %s:%s" % (target, target_tag))
+            DOCKER_CLIENT.push(target, target_tag)
+
+            print("removing - %s:%s" % (images[image], source_tag))
+            DOCKER_CLIENT.remove_image("%s:%s" % (images[image], source_tag))
+
+            print("removing - %s:%s" % (target, target_tag))
+            DOCKER_CLIENT.remove_image("%s:%s" % (target, target_tag))
+
+
 OSISM_VERSION = os.environ.get("OSISM_VERSION", "latest")
 if OSISM_VERSION == "latest":
+    process(OSISM_VERSION)
     OSISM_VERSION = os.readlink("latest").strip("/")
 
-docker_client = docker.APIClient(base_url='unix:///var/run/docker.sock')
-
-with open("etc/images.yml", "rb") as fp:
-    images = yaml.load(fp)
-
-all_docker_images = []
-for filename in glob.glob("%s/*.yml" % OSISM_VERSION):
-    with open(filename, "rb") as fp:
-        versions = yaml.load(fp)
-        all_docker_images.append(versions.get('docker_images', {}))
-        if os.path.basename(filename) == 'base.yml':
-            repository_version = versions['repository_version']
-
-for docker_images in all_docker_images:
-    for image in docker_images:
-        if IMAGES and image not in IMAGES:
-            print("skipping %s" % image)
-            continue
-
-        if image in ['rally', 'kolla-ansible', 'ceph-ansible', 'osism-ansible']:
-            continue
-
-        if not images[image][:5] == 'osism':
-            if image == 'ceph':
-                target = 'osism/ceph-daemon'
-            else:
-                target = "osism/" + images[image][images[image].find('/') + 1:]
-        else:
-            target = images[image]
-
-        target_tag = repository_version
-        if image in ['cephclient', 'openstackclient', 'ceph']:
-            target_tag = docker_images[image] + '-' + target_tag
-
-        source_tag = docker_images[image]
-        if image == 'ceph':
-            source_tag = "tag-build-master-%s-ubuntu-16.04" % source_tag
-
-        print("pulling - %s:%s" % (images[image], source_tag))
-        docker_client.pull(images[image], source_tag)
-
-        print("tagging - %s:%s" % (target, target_tag))
-        docker_client.tag("%s:%s" % (images[image], source_tag), target, target_tag)
-
-        print("pushing - %s:%s" % (target, target_tag))
-        docker_client.push(target, target_tag)
-
-        print("removing - %s:%s" % (images[image], source_tag))
-        docker_client.remove_image("%s:%s" % (images[image], source_tag))
-
-        print("removing - %s:%s" % (target, target_tag))
-        docker_client.remove_image("%s:%s" % (target, target_tag))
+process(OSISM_VERSION)
