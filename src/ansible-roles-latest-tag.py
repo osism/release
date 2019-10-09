f0e001e58b6f4939544a88af31a5fd11783ff39a
Christian Berendt <berendt@betacloud-solutions.de>
2019-10-09 15:43:40 +0200

    ansible-roles-latest-tag.py: handle repos without tags

--- src/ansible-roles-latest-tag.py
+++ src/ansible-roles-latest-tag.py
@@ -8,4 +8,10 @@ gh = github.Github(os.environ.get("GH_ACCESS_TOKEN"))
 for repository in [x for x in gh.get_organization("osism").get_repos() if x.full_name.startswith("osism/ansible") and not x.archived and not "template" in x.full_name]:
     tags = [LooseVersion(t.name) for t in repository.get_tags()]
     tags.sort()
-    print("%s %s" % (repository.full_name.ljust(40), tags[-1]))
+
+    try:
+        latest = tags[-1]
+    except IndexError:
+        latest = "none"
+
+    print("%s %s" % (repository.full_name.ljust(40), latest))
