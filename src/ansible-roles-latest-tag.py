eb512bcdffa7914fc79712bca94d668e2d791074
Christian Berendt <berendt@betacloud-solutions.de>
2019-10-09 15:41:19 +0200

    ansible-roles-latest-tag.py: use github access token

--- src/ansible-roles-latest-tag.py
+++ src/ansible-roles-latest-tag.py
@@ -1,7 +1,9 @@
 from distutils.version import LooseVersion
+import os
+
 import github
  
-gh = github.Github()
+gh = github.Github(os.environ.get("GH_ACCESS_TOKEN"))
  
 for repository in [x for x in gh.get_organization("osism").get_repos() if x.full_name.startswith("osism/ansible") and not x.archived and not "template" in x.full_name]:
     tags = [LooseVersion(t.name) for t in repository.get_tags()]
