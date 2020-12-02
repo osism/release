90b1cce443ccd3ab77ed6f253aef0757b241fb99
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-02 16:24:39 +0100

    Drop travis integration
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/travis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce
-echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
-
-echo $TRAVIS_DOCKER_PASSWORD | docker login --username="$TRAVIS_DOCKER_USERNAME" --password-stdin
