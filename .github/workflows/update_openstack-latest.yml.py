0c23a525634e31a1f19eb0e72d6a891aaeb1b4ee
Tim Beermann <beermann@betacloud-solutions.de>
2021-04-08 11:36:26 +0200

    reduce manual version updates for latest/openstack-latest.yml (#29)
    Signed-off-by: Tim Beermann <beermann@betacloud-solutions.de>

---
+++ .github/workflows/update_openstack-latest.yml.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+#
+# CI-Script to update latest/openstack-latest.yml with the latest Versions for:
+#   - elasticsearch
+#   - gnocchi
+#
+###################################################################################################
+import json
+import yaml
+import urllib.request
+import urllib.parse
+
+###################################################################################################
+# Variables
+###################################################################################################
+
+api = "https://api.github.com/repos/"
+
+
+###################################################################################################
+# Functions
+###################################################################################################
+
+def get_gnocchi_latest_tag():
+    with urllib.request.urlopen(api + "gnocchixyz/gnocchi/tags") as url:
+        data = json.loads(url.read().decode())
+        return data[0]['name']
+
+
+def get_elasticsearch_latest_tag():
+    with urllib.request.urlopen(api + "elastic/elasticsearch/tags?per_page=100", ) as url:
+        data = json.loads(url.read().decode())
+        for release in data:
+            if release['name'].startswith("v6"):
+                return release['name'][1:]
+
+
+def edit_openstack_latest(latest_elasticsearch_version, latest_gnocchi_version):
+    # load
+    with open("latest/openstack-latest.yml") as stream:
+        try:
+            loaded = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # modify
+    if loaded['infrastructure_projects']['elasticsearch'] != "":
+        loaded['infrastructure_projects']['elasticsearch'] = latest_elasticsearch_version
+    if loaded['openstack_projects']['gnocchi'] != "":
+        loaded['openstack_projects']['gnocchi'] = latest_gnocchi_version
+
+    # save
+    with open("latest/openstack-latest.yml", 'w') as stream:
+        try:
+            yaml.dump(loaded, stream, default_flow_style=False, explicit_start=True)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+###################################################################################################
+# Main
+###################################################################################################
+
+edit_openstack_latest(get_elasticsearch_latest_tag(), get_gnocchi_latest_tag())
