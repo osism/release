418321b130c04b1c0276fb4474e2c93e292d0cfd
Christian Berendt <berendt@osism.tech>
2022-10-13 10:24:41 +0200

    Autoformat python syntax with black
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,36 +1,34 @@
 extensions = []
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'OSISM'
-copyright = u'2022, OSISM GmbH'
-author = u'OSISM GmbH'
-version = u''
-release = u''
-language = 'en'
+source_suffix = ".rst"
+master_doc = "index"
+project = "OSISM"
+copyright = "2022, OSISM GmbH"
+author = "OSISM GmbH"
+version = ""
+release = ""
+language = "en"
 exclude_patterns = []
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_show_sphinx = False
 html_show_sourcelink = False
 html_show_copyright = True
-htmlhelp_basename = 'release'
+htmlhelp_basename = "release"
 html_theme_options = {
-    'display_version': False,
-    'canonical_url': 'https://release.osism.tech/',
-    'style_external_links': True,
-    'logo_only': True,
-    'prev_next_buttons_location': None
+    "display_version": False,
+    "canonical_url": "https://release.osism.tech/",
+    "style_external_links": True,
+    "logo_only": True,
+    "prev_next_buttons_location": None,
 }
 html_context = {
-    'display_github': True,
-    'github_user': 'osism',
-    'github_repo': 'release',
-    'github_version': 'main',
-    'conf_py_path': '/doc/source/'
+    "display_github": True,
+    "github_user": "osism",
+    "github_repo": "release",
+    "github_version": "main",
+    "conf_py_path": "/doc/source/",
 }
-html_logo = 'images/logo.png'
-html_static_path = [
-    '_static'
-]
+html_logo = "images/logo.png"
+html_static_path = ["_static"]
 latex_elements = {}
