2aea8f09c3ed066e023769b5c32a7d808f4cd2ec
Christian Berendt <berendt@osism.tech>
2022-10-19 14:46:21 +0200

    doc: use sphinx material theme
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,36 +1,31 @@
 extensions = []
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'OSISM'
-copyright = u'2022, OSISM GmbH'
-author = u'OSISM GmbH'
-version = u''
-release = u''
-language = 'en'
+source_suffix = ".rst"
+master_doc = "index"
+project = "OSISM"
+copyright = "2022, OSISM GmbH"
+author = "OSISM GmbH"
+version = ""
+release = ""
+language = "en"
 exclude_patterns = []
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_material"
 html_show_sphinx = False
 html_show_sourcelink = False
 html_show_copyright = True
-htmlhelp_basename = 'release'
+htmlhelp_basename = "release"
 html_theme_options = {
-    'display_version': False,
-    'canonical_url': 'https://release.osism.tech/',
-    'style_external_links': True,
-    'logo_only': True,
-    'prev_next_buttons_location': None
+    "nav_title": "OSISM Release Notes",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "globaltoc_depth": 3,
+    "globaltoc_collapse": True,
 }
-html_context = {
-    'display_github': True,
-    'github_user': 'osism',
-    'github_repo': 'release',
-    'github_version': 'main',
-    'conf_py_path': '/doc/source/'
+html_context = {}
+html_logo = "images/logo.png"
+html_title = "OSISM Release Notes"
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
 }
-html_logo = 'images/logo.png'
-html_static_path = [
-    '_static'
-]
 latex_elements = {}
